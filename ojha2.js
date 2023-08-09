7d8bee1a1f058d1f6123736adefccf113fd864a6
AVISHEK OJHA <140743024+czabhishek699@users.noreply.github.com>
2023-08-09 14:31:31 +0545

    Delete ojha2.js

--- ojha2.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("popupButton").addEventListener("click", function() {
-  document.getElementById("popup").style.display = "block";
-});
-
-document.getElementById("close").addEventListener("click", function() {
-  document.getElementById("popup").style.display = "none";
-});
